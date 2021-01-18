d780351b371d8a33fb985bde972103c2e0249362
Selim-Baskaya-22 <77636247+Selim-Baskaya-22@users.noreply.github.com>
2021-01-18 23:12:50 +0300

    Add files via upload

---
+++ PrimeNumber/AsalSayıBulma/AsalSayıBulma.cpp
@@ -0,0 +1,54 @@
+﻿
+
+#include <iostream>
+#include<locale.h>
+using namespace std;
+
+int main()
+{
+	
+	
+    setlocale(LC_ALL,"Turkish");
+    cout << "---------Asal Sayı Bulma--------" << endl;
+
+	int asalmi,sonuc;
+	basadön:
+	cout << "Sayı giriniz= ";
+	cin >> asalmi;
+	if (asalmi<=2) 
+	{
+		cout << "Girilen sayi 0-1-2 olamaz.Lütfen tekrar deneyiniz!!!" << endl;
+		goto basadön;
+	}
+	/*for (int i = 2; i <= asalmi-1;)
+	{
+
+		sonuc = asalmi % i;
+		if (sonuc!=0)
+		{
+			cout << "Sayı asaldır";
+			i += asalmi;
+		}
+		else
+		{
+			cout << "Sayı asal değildir";
+			cout << i;
+			i += asalmi;
+		}
+	}*/
+	for (int i = asalmi *(-1); i <= asalmi; i++)
+	{
+
+		sonuc = asalmi % i;
+		
+		
+		if (sonuc == 0)
+		{			
+			cout << i;			
+		}
+		
+	}
+
+
+}
+
