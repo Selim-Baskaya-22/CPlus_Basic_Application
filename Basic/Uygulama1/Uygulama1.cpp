a0c3747cab79f480d811955f56d285765f39d6c8
Selim-Baskaya-22 <77636247+Selim-Baskaya-22@users.noreply.github.com>
2021-01-18 23:15:46 +0300

    Add files via upload

---
+++ Basic/Uygulama1/Uygulama1.cpp
@@ -0,0 +1,72 @@
+﻿
+#include <iostream>
+#include <locale>
+using namespace std;  
+int main()
+{
+
+	int a, b = 1, sonuc = 0;
+	cin >> a;
+	cin >> b;
+	for (int i = 0; i < 10; i++)
+	{  
+      
+      
+		sonuc = a + b;
+    // sonuc değişkeninin değerini, b değişkenine atıyoruz.  
+			cout << sonuc << endl;
+        
+    // a ile b değerlerini topluyoruz ve sonuc değişkenine atıyoruz.  
+			a = b;
+			// b değişkeninin değerini, a değişkenine atıyoruz.  
+
+			b = sonuc;
+        
+    // sonuc değerini konsola yazdırıyoruz.  
+              
+        }      
+
+
+
+
+
+
+
+
+
+
+
+	//int sayi;
+	//cin >> sayi;
+	//int sonuc=1 ;
+	//for (int i = sayi; i > 1; i--)
+	//{
+	//	sonuc *= i;
+	//}
+	//cout << sonuc;
+	//int i;
+ //   setlocale(LC_ALL,"Turkish");
+	//for ( i = 1; i < 10; i+=2)
+	//{
+	//	cout << i;
+	//}
+	//cout << endl;
+	//cout << i;
+	//int toplam = 0;
+	/*for (int a = 0; a <=10; a++)
+	{
+		if ( a % 2 != 0)
+		{
+			cout << a;
+		}
+
+	}*/
+	//cout << toplam;
+	/*for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 || i % 5 = 0 )
+		{
+			cout << i << endl;
+		}
+	}*/
+}
---
+++ PointArray/pointeraray/pointeraray.cpp
@@ -0,0 +1,40 @@
+﻿#include <locale.h>
+#include <iostream>
+using namespace std;
+void rastgele();
+int main()
+{
+    setlocale(LC_ALL, "Turkish");
+    rastgele();
+    /*int dizi[4]{1,2,3,4};
+        
+        for (int i = 0; i <=4; i++)
+        {
+            cout << dizi[i]<<"a";
+        }*/
+
+     /*   int dizii[] = { 1,2,3,4,5 };
+        int* ptr2 = dizii;
+        cout << ptr2[0];*/
+}
+void rastgele()
+{
+    srand(time(0));
+    int sayi = 1+rand() % 100;
+    int sayidizi[10];
+    for (int i = 0; i <= 10; i++)
+    {
+        sayidizi[i] = sayi;
+    }
+   
+        for (int a = 0; a < 11; a++)
+        {
+            if (sayi % 4 == 0)
+            {
+                cout << sayidizi[a];
+            }
+        }
+    
+
+}
+
