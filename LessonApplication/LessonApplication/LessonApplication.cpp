f2dbda03da598237df40b0c76911cf527d70f8ba
Selim-Baskaya-22 <77636247+Selim-Baskaya-22@users.noreply.github.com>
2021-01-18 23:18:51 +0300

    Add files via upload

---
+++ LessonApplication/LessonApplication/LessonApplication.cpp
@@ -0,0 +1,20 @@
+﻿#include <locale>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL,"Turkish");
+    int s1,çarp=1;   
+    do
+    {
+        cout << "Sayı giriniz=";
+        cin >> s1; 
+        if (s1 != 0 ? çarp = çarp * s1:çarp);
+       
+   
+        
+    } while (s1 != 0);  
+    cout << "Sonuç= "<<çarp;
+  
+}
+
---
+++ StarFor/yıldız.cpp
@@ -0,0 +1,9 @@
+﻿#include <locale.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Turkish");
+    cout << "Hello Worİıld!\n";
+}
+
